9a90a9c90a37f6146f3ab338f092365084fb9655
Aster <letranhieu.ph26@gmail.com>
2020-12-08 19:58:07 -0500

    update

--- Code_AashishNair/download_data.py
+++
@@ -1,7 +0,0 @@
-import os
-import pandas as pd
-import cv2
-
-if "train" not in os.listdir():
-    os.system("wget https://storage.googleapis.com/letranhieu-bucket-data/data.zip")
-    os.system("unzip data.zip")
